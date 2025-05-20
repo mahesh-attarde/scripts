ed7ac3e114b6d97280aac7674fedf56a88882e05
Mahesh-Attarde <mahesh.attarde@intel.com>
2025-05-20 20:30:41 +0530

    Create cpuid.cpp

---
+++ cpuid.cpp
@@ -0,0 +1,22 @@
+// compile
+// g++ cpuid.cpp
+// RUN: sde -nvl -- ./a.out 
+// RUN: ./a.out
+#include <stdio.h>
+
+void cpuid(int code, unsigned int *a, unsigned int *b, unsigned int *c, unsigned int *d) {
+    __asm__ volatile("cpuid"
+                     : "=a"(*a), "=b"(*b), "=c"(*c), "=d"(*d)
+                     : "0"(code));
+}
+
+int main() {
+    unsigned int eax, ebx, ecx, edx;
+    cpuid(1, &eax, &ebx, &ecx, &edx);
+
+    unsigned int family = ((eax >> 8) & 0xF) + ((eax >> 20) & 0xFF);
+    unsigned int model = ((eax >> 4) & 0xF) + ((eax >> 16) & 0xF);
+    printf("This CPU processor. %x %x\n", family, model);
+
+    return 0;
+}
